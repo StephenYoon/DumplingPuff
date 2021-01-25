08e1d9d94935d7264813868af0f4e56b82b68f18
Stephen Yoon <yoonic@gmail.com>
2021-01-24 19:37:14 -0500

    Angular social login, Web API settings endpoint

---
+++ DumplingPuff.Web/Controllers/AppSettingsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DumplingPuff.Web.Models.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DumplingPuff.Web.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class AppSettingsController : Controller
+    {
+        IAppSettings _appSettings;
+
+        public AppSettingsController(IAppSettings appSettings)
+        {
+            _appSettings = appSettings;
+        }
+
+        // GET: AppSettings
+        [HttpGet("v1/[controller]/settings")]
+        public IActionResult Index()
+        {
+            var settings = new JsonResult(_appSettings)
+            {
+                StatusCode = 201
+            };
+            return settings;
+        }
+    }
+}
---
+++ DumplingPuff.Web/Models/Configuration/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DumplingPuff.Web.Models.Configuration
+{
+    public class AppSettings : IAppSettings
+    {
+        public static string EnvironmentCode => System.Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "dev";
+        public string Environment => EnvironmentCode;
+        public string AuthenticationGoogleClientId { get; set; }
+        public string AuthenticationGoogleClientSecret { get; set; }
+    }
+}
