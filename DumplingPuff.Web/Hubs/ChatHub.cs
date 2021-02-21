ebea8e6c02d8fc3b87ca944b6b328b0b3c508e77
Stephen Yoon <yoonic@gmail.com>
2021-02-21 17:59:36 -0500

    Add chathub method

--- DumplingPuff.Web/Hubs/ChatHub.cs
+++ DumplingPuff.Web/Hubs/ChatHub.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using DumplingPuff.Web.Models.Chat;
+using DumplingPuff.Web.Services;
 
 namespace DumplingPuff.Web.Hubs
 {
     public class ChatHub : Hub
     {
+        private IChatService _chatService;
+
+        public ChatHub(IChatService chatService)
+        {
+            _chatService = chatService;
+        }
+
         public override Task OnConnectedAsync() => 
             Clients.All.SendAsync("broadcastSystemMessage", $"{Context.User.Identity.Name} JOINED");
 
-        public Task BroadcastMessage(string name, string message) =>
-            Clients.All.SendAsync("broadcastMessage", name, message);
+        public Task UpdateChatGroup(string groupId, ChatMessage chatMessage)
+        {
+            _chatService.AddChatMessageToGroup(groupId, chatMessage);
+            var broadcastContent = _chatService.GetChatGroup(groupId);
+            return Clients.All.SendAsync("broadcastChatGroup", broadcastContent);
+        }
 
         public Task Echo(string name, string message) =>
             Clients.Client(Context.ConnectionId)
